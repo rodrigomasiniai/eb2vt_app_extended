afb78a9173906d6e7c45453544c3c1d4e34223a3
Rodrigo Masini <104578949+rodrigomasiniai@users.noreply.github.com>
2023-07-24 19:07:34 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,15 @@
 mkdir -p ~/.streamlit/
 
-echo "
-
-[server]\\n
-
-headless = true\\n
-
-enableCORS=false\\n
-
-port = $PORT\\n
-
-\\n
-
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
 " > ~/.streamlit/config.toml
 
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/secrets.toml
